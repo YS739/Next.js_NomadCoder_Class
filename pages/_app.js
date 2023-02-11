b537063b13db420c0db2e209649ea162e448e109
YS739 <suk921@gmail.com>
2023-02-11 18:01:06 +0900

    test: Seo 컴포넌트 활용

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,11 @@
-import NavBar from "@/components/NavBar";
+import Layout from "@/components/Layout";
+import "../styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      {/* global만 붙이면 전역으로 사용 가능 */}
-      <style jsx global>{`
-        .active {
-          color: forestgreen;
-          font-size: 24px;
-        }
-      `}</style>
-    </>
+    </Layout>
   );
 };
 
